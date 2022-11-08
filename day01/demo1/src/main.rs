1e64290a50a2809d15e1c65f9720d4e735f8a64a
jiangzhou <jz2630930772@163.com>
2022-11-08 08:56:36 +0800

    rust学习第一天：变量

--- day01/demo1/src/main.rs
+++ day01/demo1/src/main.rs
@@ -1,3 +1,20 @@
+const MAX_POINTS:u32 = 10000;
 fn main() {
-    println!("Hello, world!");
+    //1.变量的定义
+    //定义变量使用let，如果要定义可变变量需要使用mut
+    let a = 1;
+    println!("a={}", a);
+
+    let mut b:u32 = 1;
+    println!("b={}", b);
+
+    b=2;
+    println!("b={}", b);
+
+    //2.隐藏
+    let mut b:f32 = 1.1;
+    println!("b={}", b);
+
+    //3.常量
+    println!("MAX_POINTS={}", MAX_POINTS);
 }
